71f3ed6b27a461782c95f3b304eacf8ed5f736f9
Stefko1998 <Simjanovski998@gmail.com>
2024-03-21 16:49:23 +0100

    input form for email verification

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,18 @@
+import './Input.css';
+
+const Input = () => {
+  return (
+    <>
+      <form>
+        <label>
+          Email address
+        </label>
+        <input
+          type="email"
+          placeholder='Enter your e-mail' />
+      </form>
+    </>
+  )
+}
+
+export default Input;
