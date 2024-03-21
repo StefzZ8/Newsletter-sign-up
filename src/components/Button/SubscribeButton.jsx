5a387ed4824dcb836600a1b6cb92091d3b310ca9
Stefko1998 <Simjanovski998@gmail.com>
2024-03-21 16:47:30 +0100

    styling button component

---
+++ src/components/Button/SubscribeButton.jsx
@@ -0,0 +1,11 @@
+import './SubscribeButton.css';
+
+const SubscribeButton = () => {
+  return (
+    <>
+      <button>Subscribe to monthly newsletter</button>
+    </>
+  )
+}
+
+export default SubscribeButton;
