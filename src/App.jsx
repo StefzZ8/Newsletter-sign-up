5ce412ce865e0f597529cc70a27227664af8214d
Stefko1998 <Simjanovski998@gmail.com>
2024-04-12 12:17:44 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import './App.scss';
-import illustrationImg from './assets/images/illustration-sign-up-desktop.svg';
-import checkIcon from './assets/images/icon-list.svg';
-import Input from './components/Input/Input';
-import Button from './components/Button/SubscribeButton';
+import Modal from './components/Modal/Modal';
+import Main from './components/Main';
+
 
 const App = () => {
   return (
     <>
-      <section>
-        <div className='container'>
-          <div className='main-section'>
-            <h1>Stay updated!</h1>
-            <p className='first-p'>Join 60,000+ product managers receiving monthly updates on:</p>
-            <p><img src={checkIcon} alt="success" /> <span>Product discovery and building what matters</span></p>
-            <p><img src={checkIcon} alt="success" /> <span>Measuring to ensure updates are a success</span></p>
-            <p><img src={checkIcon} alt="success" /> <span>And much more!</span></p>
-            <div>
-              <Input />
-              <Button />
-            </div>
-          </div>
-          <div>
-            <img src={illustrationImg} alt="illustration-sign-up" className='img' />
-          </div>
-        </div>
-      </section>
+      <Main />
+      {/* <Modal /> */}
     </>
   )
 }
